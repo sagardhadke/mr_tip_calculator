3055ef5c08686f5110aa8b1eaf9ff7e2616da338
Sagar Dhadke <peaceunique2003@gmail.com>
2025-03-26 13:50:36 +0530

    Remove All Unwanted Code and folders

---
+++ lib/mrTip.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MrTipCalculator extends StatelessWidget {
+  const MrTipCalculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
